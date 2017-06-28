8af7f0b37ef2c5971f8ea894295f5b251a722157
Fung <funglam@astri.org>
2017-06-28 14:54:45 +0800

    clean up

--- app/src/main/java/com/androidfung/altrestest/MainActivity.java
+++ app/src/main/java/com/androidfung/altrestest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.androidfung.altrestest;
 
+import android.content.res.Configuration;
 import android.os.Build;
 import android.os.LocaleList;
+import android.support.v4.os.LocaleListCompat;
+import android.support.v4.view.ViewConfigurationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,11 +20,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            Log.d(TAG, LocaleList.getDefault().toString());
-            Log.d(TAG, String.valueOf(LocaleList.getDefault().getFirstMatch(new String[]{"zh-hans-tw"})));
-            Log.d(TAG, Locale.TRADITIONAL_CHINESE.toLanguageTag());
-        }
+        Log.d(TAG, LocaleListCompat.getDefault().toString());
+//
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//            Log.d(TAG, LocaleList.getDefault().toString());
+//        }else{
+//
+//        }
+//        Log.d(TAG, Locale.getDefault().toString());
+        Log.d(TAG, getString(R.string.locale_used));
     }
 }
